bf60180391edb241c389dd797c7e08dd06328ec2
Chaman Deep Singh <chamandeepsingh019980@gmail.com>
2022-12-07 17:16:10 +0530

    Initial Commit

---
+++ new 1.js
@@ -0,0 +1,364 @@
+
+        $(document).ready(function() {
+            $('#up1').click(function() {
+                $("html, body").animate({
+                    scrollTop: 700
+                }, 0);
+                return true;
+            });
+
+        });
+        $(document).ready(function() {
+            $('#up2').click(function() {
+                $("html, body").animate({
+                    scrollTop: 700
+                }, 0);
+                return true;
+            });
+
+        });
+ 
+    // <!--0 -->
+    
+        function img() {
+            var img = document.getElementById('img');
+            var imgo = document.getElementById('imgo');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function img_close() {
+            var imgi = document.getElementById('img');
+            var imgj = document.getElementById('imgo');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+   
+    // <!-- _r -->
+   
+        function img_r() {
+            var img_r = document.getElementById('img-r');
+            var imgo_r = document.getElementById('imgo-r');
+            img_r.classList.add("btn-hover-r");
+            imgo_r.classList.add("op");
+        }
+
+        function img_r_close() {
+            var imgi = document.getElementById('img-r');
+            var imgj = document.getElementById('imgo-r');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+    
+    // <!-- 1 -->
+   
+        function img1() {
+            var img = document.getElementById('img1');
+            var imgo = document.getElementById('imgo1');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function img1_close() {
+            var imgi = document.getElementById('img1');
+            var imgj = document.getElementById('imgo1');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+  
+    // <!-- _r1 -->
+   
+        function img_r1() {
+            var img = document.getElementById('img_r1');
+            var imgo = document.getElementById('imgo_r1');
+            img.classList.add("btn-hover-r");
+            imgo.classList.add("op");
+        }
+
+        function img_r1_close() {
+            var imgi = document.getElementById('img_r1');
+            var imgj = document.getElementById('imgo_r1');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+  
+    // <!-- 4 or -->
+    // <!-- 2 -->
+  
+        function img2() {
+            var img = document.getElementById('img2');
+            var imgo = document.getElementById('imgo2');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function img2_close() {
+            var imgi = document.getElementById('img2');
+            var imgj = document.getElementById('imgo2');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+
+    // <!-- _r2 -->
+   
+        function img_r2() {
+            var img_r = document.getElementById('img-r2');
+            var imgo_r = document.getElementById('imgo-r2');
+            img_r.classList.add("btn-hover-r");
+            imgo_r.classList.add("op");
+        }
+
+        function img_r2_close() {
+            var imgi = document.getElementById('img-r2');
+            var imgj = document.getElementById('imgo-r2');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+    
+    // <!-- 3 -->
+   
+        function img3() {
+            var img = document.getElementById('img3');
+            var imgo = document.getElementById('imgo3');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function img3_close() {
+            var imgi = document.getElementById('img3');
+            var imgj = document.getElementById('imgo3');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+   
+    // <!-- _r3 -->
+  
+        function img_r3() {
+            var img = document.getElementById('img_r3');
+            var imgo = document.getElementById('imgo_r3');
+            img.classList.add("btn-hover-r");
+            imgo.classList.add("op");
+        }
+
+        function img_r3_close() {
+            var imgi = document.getElementById('img_r3');
+            var imgj = document.getElementById('imgo_r3');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+   
+    // <!-- 4 -->
+ 
+        function img4() {
+            var img = document.getElementById('img4');
+            var imgo = document.getElementById('imgo4');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function img4_close() {
+            var imgi = document.getElementById('img4');
+            var imgj = document.getElementById('imgo4');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+	// <!-- 2 or -->
+    // <!-- _r4 -->
+   
+        function img_r4() {
+            var img = document.getElementById('img_r4');
+            var imgo = document.getElementById('imgo_r4');
+            img.classList.add("btn-hover-r");
+            imgo.classList.add("op");
+        }
+
+        function img_r4_close() {
+            var imgi = document.getElementById('img_r4');
+            var imgj = document.getElementById('imgo_r4');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+    
+  // DUSRA PAGE START HO GA
+    // <!--0 -->
+    
+        function imga() {
+            var img = document.getElementById('imga');
+            var imgo = document.getElementById('imgoa');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function imga_close() {
+            var imgi = document.getElementById('imga');
+            var imgj = document.getElementById('imgoa');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+   
+    // <!-- _r -->
+   
+        function imga_r() {
+            var img_r = document.getElementById('imga-r');
+            var imgo_r = document.getElementById('imgoa-r');
+            img_r.classList.add("btn-hover-r");
+            imgo_r.classList.add("op");
+        }
+
+        function imga_r_close() {
+            var imgi = document.getElementById('imga-r');
+            var imgj = document.getElementById('imgoa-r');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+    
+    // <!-- 1 -->
+   
+        function imga1() {
+            var img = document.getElementById('imga1');
+            var imgo = document.getElementById('imgoa1');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function imga1_close() {
+            var imgi = document.getElementById('imga1');
+            var imgj = document.getElementById('imgoa1');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+  
+    // <!-- _r1 -->
+   
+        function imga_r1() {
+            var img = document.getElementById('imga_r1');
+            var imgo = document.getElementById('imgoa_r1');
+            img.classList.add("btn-hover-r");
+            imgo.classList.add("op");
+        }
+
+        function imga_r1_close() {
+            var imgi = document.getElementById('imga_r1');
+            var imgj = document.getElementById('imgoa_r1');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+  
+    // <!-- 4 or -->
+    // <!-- 2 -->
+  
+        function imga2() {
+            var img = document.getElementById('imga2');
+            var imgo = document.getElementById('imgoa2');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function imga2_close() {
+            var imgi = document.getElementById('imga2');
+            var imgj = document.getElementById('imgoa2');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+
+    // <!-- _r2 -->
+   
+        function imga_r2() {
+            var img_r = document.getElementById('imga-r2');
+            var imgo_r = document.getElementById('imgoa-r2');
+            img_r.classList.add("btn-hover-r");
+            imgo_r.classList.add("op");
+        }
+
+        function imga_r2_close() {
+            var imgi = document.getElementById('imga-r2');
+            var imgj = document.getElementById('imgoa-r2');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+    
+    // <!-- 3 -->
+   
+        function imga3() {
+            var img = document.getElementById('imga3');
+            var imgo = document.getElementById('imgoa3');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function imga3_close() {
+            var imgi = document.getElementById('img3');
+            var imgj = document.getElementById('imgo3');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+   
+    // <!-- _r3 -->
+  
+        function imga_r3() {
+            var img = document.getElementById('imga_r3');
+            var imgo = document.getElementById('imgoa_r3');
+            img.classList.add("btn-hover-r");
+            imgo.classList.add("op");
+        }
+
+        function imga_r3_close() {
+            var imgi = document.getElementById('imga_r3');
+            var imgj = document.getElementById('imgoa_r3');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+   
+    // <!-- 4 -->
+ 
+        function imga4() {
+            var img = document.getElementById('imga4');
+            var imgo = document.getElementById('imgoa4');
+            img.classList.add("btn-hover");
+            imgo.classList.add("op");
+        }
+
+        function imga4_close() {
+            var imgi = document.getElementById('imga4');
+            var imgj = document.getElementById('imgoa4');
+            imgi.classList.remove("btn-hover");
+            imgj.classList.remove("op");
+        }
+	// <!-- 2 or -->
+    // <!-- _r4 -->
+   
+        function imga_r4() {
+            var img = document.getElementById('imga_r4');
+            var imgo = document.getElementById('imgoa_r4');
+            img.classList.add("btn-hover-r");
+            imgo.classList.add("op");
+        }
+
+        function imga_r4_close() {
+            var imgi = document.getElementById('imga_r4');
+            var imgj = document.getElementById('imgoa_r4');
+            imgi.classList.remove("btn-hover-r");
+            imgj.classList.remove("op");
+        }
+		
+        var i = 0;
+        var txt = 'YOUR TAILOR MADE WORDPRESS THEME';
+        var speed = 50;
+        typeWriter();
+
+        function typeWriter() {
+            if (i < txt.length) {
+                document.getElementById("demo").innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+        }
+    
+   
+        new WOW().init();
+    
+    
+	$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+  
